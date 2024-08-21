28c0f187ad430f2107e96df1b0efc22feec57d6c
gargipedhekar <gargi27.pedhekar@gmail.com>
2024-08-21 15:19:09 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,14 @@
+// Gargi Pedhekar
+//23070123052
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i =0 ; i <= 10 ; i++ ){
+        if(i%2==0){
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
